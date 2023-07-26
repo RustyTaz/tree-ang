f11f207abe910fc9a983271c995eeeb90baec183
Rustam Tazyaev <rustytazyaev@gmail.com>
2023-07-26 15:39:37 +0400

    html+id

--- src/app/tree/tree.component.ts
+++ src/app/tree/tree.component.ts
@@ -9,44 +9,17 @@ interface TreeNode {
   templateUrl: './tree.component.html',
   styleUrls: ['./tree.component.css'],
 })
-
-// Тут я отображаю рекурсивно полученное дерево + логика открытия закрытия узлов списка
 export class TreeComponent {
   @Input() dataTree: any;
 
   tree: TreeNode | undefined;
   fields = ['enterpriseName', 'vehicleTypeName', 'modelName', 'name'];
-  public expandedNodes: string[] = [];
 
   ngOnInit(): void {
-    this.tree = this.buildTree(
-      this.dataTree,
-      this.fields
-    );
+    this.tree = this.buildTree(this.dataTree, this.fields);
     console.log(this.tree);
   }
 
-  get treeKeys(): string[] {
-    return this.tree ? Object.keys(this.tree) : [];
-  }
-
-  toggleNode(node: string): void {
-    if (this.nodeIsExpanded(node)) {
-      this.expandedNodes = this.expandedNodes.filter((n) => n !== node);
-    } else {
-      this.expandedNodes.push(node);
-    }
-  }
-
-  nodeIsExpanded(node: string): boolean {
-    return this.expandedNodes.includes(node);
-  }
-
-  isTreeNode(obj: any): obj is TreeNode {
-    return typeof obj === 'object' && obj !== null && !Array.isArray(obj);
-  }
-
-  //метод построения дерева разной глубины в зависимости от входных параметров
   buildTree(data: any[], fields: string[]): TreeNode {
     const tree: TreeNode = {};
 
@@ -57,7 +30,7 @@ export class TreeComponent {
         const value = item[field];
 
         if (!currentNode[value]) {
-          currentNode[value] = index === fields.length - 1 ? value : {};
+          currentNode[value] = index === fields.length - 1 ? item.id : {};
         }
 
         currentNode = currentNode[value] as TreeNode;
@@ -66,4 +39,9 @@ export class TreeComponent {
 
     return tree;
   }
+
+  nodeKeys(node: TreeNode | string): string[] {
+    return typeof node === 'object' ? Object.keys(node) : [];
+  }
 }
+
